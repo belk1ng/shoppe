4b572f4e1c24848d4776cebe7d499dc72f173852
belk1ng <be1kindmit@yandex.ru>
2025-02-20 19:12:35 +0300

    feat: 🎸 add elements to catalog page
    Add `Input` component. Add product cards rendering on the catalog page.

---
+++ src/app/catalog/components/filter-form/FilterForm.tsx
@@ -0,0 +1,22 @@
+import { Search } from "@/assets/icons";
+import { Input } from "@/components/input";
+import { cn } from "@/lib/cn";
+
+export interface FilterFormProps {
+  className?: string;
+}
+
+const block = cn("productsFilterForm");
+
+export const FilterForm = ({ className }: FilterFormProps) => {
+  return (
+    <form className={block("", [className])} role="search">
+      <Input
+        endIcon={<Search />}
+        label="Название"
+        name="name"
+        placeholder="Поиск"
+      />
+    </form>
+  );
+};
