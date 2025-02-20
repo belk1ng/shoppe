89e09f5065556b61ff2a28f35778945481c354b4
belk1ng <be1kindmit@yandex.ru>
2025-02-20 19:12:35 +0300

    feat: 🎸 add elements to catalog page
    Add `Input` component. Add product cards rendering on the catalog page.

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { type InputHTMLAttributes, type ReactNode, useId } from "react";
+import { cn } from "@/lib/cn";
+import "./input.scss";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+}
+
+const block = cn("input");
+
+export const Input = ({
+  id,
+  className,
+  label,
+  startIcon,
+  endIcon,
+  ...props
+}: InputProps) => {
+  const _id = useId();
+
+  return (
+    <div className={block("", [className])}>
+      {startIcon}
+      <label className="visuallyHidden" htmlFor={id ?? _id}>
+        {label}
+      </label>
+      <input className={block("field")} id={id ?? _id} {...props} />
+      {endIcon}
+    </div>
+  );
+};
