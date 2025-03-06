a9a6671ccb031cfca0b9124ad55da6c79efb3a1d
belk1ng <be1kindmit@yandex.ru>
2025-03-06 21:20:48 +0300

    feat: 🎸 add products carousel
    Add component `Carousel`. Add component `ProductSlide`.

---
+++ src/entities/product/ui/product-slide/ProductSlide.tsx
@@ -0,0 +1,42 @@
+import Image, { type StaticImageData } from "next/image";
+import { cn } from "@/shared/lib";
+import { Button } from "@/shared/ui";
+import "./product-slide.scss";
+
+export interface ProductSlideProps {
+  title: string;
+  price: number;
+  imageSrc: string | StaticImageData;
+  cta?: string;
+}
+
+const block = cn("productSlide");
+
+export function ProductSlide({
+  title,
+  price,
+  imageSrc,
+  cta,
+}: ProductSlideProps) {
+  return (
+    <div className={block()}>
+      <h2 className={block("title")} lang="en">
+        {title}
+      </h2>
+      <span className={block("price")}>$ {price.toFixed(2)}</span>
+      <Button className={block("cta")} variant="outlined-white">
+        {cta ?? "Смотреть"}
+      </Button>
+
+      <Image
+        alt={`Изображение продукта ${title}`}
+        className={block("image")}
+        fill
+        priority
+        sizes="100%"
+        src={imageSrc}
+        style={{ objectFit: "cover" }}
+      />
+    </div>
+  );
+}
--- steiger.config.ts
+++ steiger.config.ts
@@ -4,7 +4,11 @@ import { defineConfig } from "steiger";
 export default defineConfig([
   ...fsd.configs.recommended,
   {
-    files: ["./src/shared/styles/**", "./src/shared/typings/**"],
+    files: [
+      "./src/shared/styles/**",
+      "./src/shared/typings/**",
+      "./src/shared/assets/**",
+    ],
     rules: {
       "fsd/public-api": "off",
     },
